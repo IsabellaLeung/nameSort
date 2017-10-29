dbae10fc7e2ed838565f38c90af9454dcc2a3903
Isabella <33117101+IsabellaLeung@users.noreply.github.com>
2017-10-29 21:35:35 +1000

    Code reshuffle

--- nameSort/Program.cs
+++ nameSort/Program.cs
@@ -9,51 +9,78 @@ namespace nameSort
     {
         static void Main(string[] args)
         {
+            //Check input is not nothing
             if (args == null || args.Length == 0)
             {
                 Console.WriteLine("You haven't given me anything to sort!");
                 return;
             } 
 
-            //Read each line of the file into a string array
-            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            //Input file
+            string[] fileToOpen = File.ReadAllLines(args[0]);
 
-            //List to store each line/name in 
-            List <string> namesList = new List<string>();
-            string path = Directory.GetCurrentDirectory() + @"\" + "sorted-names-list.txt";
+            //Read file
+            var unsortedList = ListOfNames.NamesList(fileToOpen);
+            //Sort file
+            var sortedList = SortingNames.SortNames(unsortedList);
+
+            //Save file
+            SaveToFile.SaveSortedList(sortedList);
+
+            //Keep the console window open
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+
+    public class ListOfNames
+    {
+        public static List<string> NamesList(string[] textInFile)
+        {
+            List<string> nameList = new List<string>();
 
             //Add each line of the file to the name list then sort by last name
-            foreach (string line in lines)
-            {
-                //Make sure the name has at least 2 names (first/last) but does not exceed 4 (3first/1last max)
+            foreach (string line in textInFile) {
+                //Make sure the name has at least 2 names (first/last) but does not exceed 4 (3first/1last max) 
                 if (line.Count(Char.IsWhiteSpace) <= 3 && line.Count(Char.IsWhiteSpace) >= 1)
                 {
-                    namesList.Add(line); 
+                    nameList.Add(line);
                 }
                 else
                 {
                     Console.WriteLine("Invalid name inserted");
                 }
             }
+            return nameList;
+        }
+    }
 
+    public class SortingNames
+    {
+        public static List<string> SortNames(List<string> listToSort)
+        {
             //Order the names list first by surname, then by first name if surnames are the same
-            namesList = namesList.OrderBy(x => x.Split(' ').Last()).ThenBy(x => x).ToList();
+            listToSort = listToSort.OrderBy(x => x.Split(' ').Last()).ThenBy(x => x).ToList();
+            //Print to Console
+            listToSort.ForEach(i => Console.WriteLine("{0}\n", i));
+            return listToSort;
+        }
+    }
 
-            //Create a new file to store ordered names in
+    public class SaveToFile 
+    {
+        public static List<string> SaveSortedList(List<string> fileToSave)
+        {
+            //Save file in same dir as unsorted list
+            string path = Directory.GetCurrentDirectory() + @"\" + "sorted-names-list.txt";
             using (StreamWriter newNamesList = File.CreateText(path))
             {
-                foreach (string orderedName in namesList)
+                foreach (string orderedName in fileToSave)
                 {
-                    newNamesList.WriteLine(orderedName);
-                } 
+                    newNamesList.WriteLine(orderedName); 
+                }
             }
-
-            //Write ordered names to console
-            namesList.ForEach(i => Console.WriteLine("{0}\n", i));
-
-            //Keep the console window open
-            Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+            return fileToSave;
         }
     }
 }
